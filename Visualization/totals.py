509b773899f66a5128207a953d7fd8af496962de
Tse <michytsemichy@gmail.com>
2018-09-05 08:44:17 -0700

    Organized files

---
+++ Visualization/totals.py
@@ -0,0 +1,14 @@
+"""
+
+This program generates the bar graphs for the compound total lists
+(MON, RON, TSI, Type, CN). There are two bar graphs created:
+
+(1) Bar graph displaying the amount of total numbers gathered as 
+    opposed to the original amount of numbers given. 
+
+(2) Bar graph displaying the amount of new compounds gathered as
+    opposed to the original amount of compounds given. 
+
+"""
+
+
