7640ad05ab3027e1ac062d625999f828a14ed797
Tse <michytsemichy@gmail.com>
2018-09-04 17:55:22 -0700

    Added comments

--- Screening/pcid.py
+++ Screening/pcid.py
@@ -4,8 +4,14 @@ import io
 
 """
 
-Currently using compounds from APIDATA, so we should iterate through compound names
-and get the pubchem cids for each compound. :)
+This program will take the compounds listed in names.txt (see example), then
+gather the PubChem IDs for each compound and print them to the console.
+
+Please note the current format prints the name of the compound, then its 
+respective PubChem ID (there may be more than one ID number found). 
+
+However, the user can choose to change the output format to what is more
+useful/needed. 
 
 """
 counter = 0
@@ -16,9 +22,10 @@ fileObject = io.open("names.txt", encoding='utf-8', mode='r+')
 totaltext = fileObject.readlines()
 for name in totaltext:
     counter += 1
-    print(counter) 
+    # print(counter) 
+    # print(name) 
     for compound in get_compounds(name, 'name'):
-        print(compound.cid)
+        print name, compound.cid, "\n" 
     
         # if compound.cid != None:
         #     # print(compound.cid)
