df706160b868f645dbe63a20cdfbc2187eecfd1b
Tse <michytsemichy@gmail.com>
2018-09-06 08:57:16 -0700

    Update

--- Visualization/totals.py
+++ Visualization/totals.py
@@ -31,38 +31,9 @@ ax.xaxis.set_ticks_position('bottom')
 
 "(1) Make the bar graph for total amount of numbers gathered"
 
-### Numbers for the original and new bars 
-originaldata = (188, 0, 323, 98, 0) 
-newdata = (748, 665, 853, 70, 742) 
-
-### Locations of x tick marks and width of bars
-xticklocations = np.arange(5)   
-width = 0.4       
-
-### Make the bar graphs
-p1 = plt.bar(xticklocations, originaldata, width, align='center', color='pink')
-p2 = plt.bar(xticklocations, newdata, width, bottom=originaldata, align='center', color='green')   
-
-### Make the labels/title
-plt.ylabel('Amount of Compound Data Found\n')
-# plt.xlabel('Category') 
-plt.title(r'$\mathrm{Total\ New\ Data\ vs\ Original\ Data}$', fontsize=20) 
-# plt.title('New Data vs Original Data', fontsize=20)
-plt.xticks(xticklocations, ('RON', 'MON', 'CN', 'TSI', 'Type'))
-plt.yticks(np.arange(0, 1300, 100))
-plt.legend((p1[0], p2[0]), ('Original Data', 'Total New Data'))
-
-### Display/Save the graph
-# plt.show() 
-plt.savefig('/Users/mtse/Documents/CompleteListGraphs/TotalCompounds.eps', format='eps', dpi=1000) 
-
-
-
-"(2) Make the bar graph for total amount of new compounds gathered"
-
 # ### Numbers for the original and new bars 
 # originaldata = (188, 0, 323, 98, 0) 
-# newdata = (256, 348, 504, 70, 742) 
+# newdata = (748, 665, 853, 70, 328)  
 
 # ### Locations of x tick marks and width of bars
 # xticklocations = np.arange(5)   
@@ -75,12 +46,41 @@ plt.savefig('/Users/mtse/Documents/CompleteListGraphs/TotalCompounds.eps', forma
 # ### Make the labels/title
 # plt.ylabel('Amount of Compound Data Found\n')
 # # plt.xlabel('Category') 
-# plt.title(r'$\mathrm{Unique\ New\ Data\ vs\ Original\ Data}$', fontsize=20) 
+# plt.title(r'$\mathrm{Total\ New\ Numerical\ Data\ vs\ Original\ Data}$', fontsize=20) 
 # # plt.title('New Data vs Original Data', fontsize=20)
-# plt.xticks(xticklocations, ('RON', 'MON', 'CN', 'TSI', 'Type'))
-# plt.yticks(np.arange(0, 1000, 100))
-# plt.legend((p1[0], p2[0]), ('Original Data', 'Unique New Data')) 
+# plt.xticks(xticklocations, ('RON', 'MON', 'CN', 'TSI', 'Sensitivity'))
+# plt.yticks(np.arange(0, 1300, 100))
+# plt.legend((p1[0], p2[0]), ('Original Data', 'Total New Data'))
 
 # ### Display/Save the graph
 # # plt.show() 
-# plt.savefig('/Users/mtse/Documents/CompleteListGraphs/UniqueData.eps', format='eps', dpi=1000) 
+# plt.savefig('/Users/mtse/Documents/SensitivityTotals/TotalNumerical.eps', format='eps', dpi=1000) 
+
+
+
+"(2) Make the bar graph for total amount of new compounds gathered"
+
+### Numbers for the original and new bars 
+originaldata = (188, 0, 323, 98, 0) 
+newdata = (256, 348, 504, 70, 328)   
+
+### Locations of x tick marks and width of bars
+xticklocations = np.arange(5)   
+width = 0.4       
+
+### Make the bar graphs
+p1 = plt.bar(xticklocations, originaldata, width, align='center', color='pink')
+p2 = plt.bar(xticklocations, newdata, width, bottom=originaldata, align='center', color='green')   
+
+### Make the labels/title
+plt.ylabel('Amount of Compound Data Found\n')
+# plt.xlabel('Category') 
+plt.title(r'$\mathrm{Unique\ New\ Numerical\ Data\ vs\ Original\ Data}$', fontsize=20) 
+# plt.title('New Data vs Original Data', fontsize=20)
+plt.xticks(xticklocations, ('RON', 'MON', 'CN', 'TSI', 'Sensitivity'))
+plt.yticks(np.arange(0, 1000, 100))
+plt.legend((p1[0], p2[0]), ('Original Data', 'Unique New Data')) 
+
+### Display/Save the graph
+# plt.show() 
+plt.savefig('/Users/mtse/Documents/SensitivityTotals/UniqueNumericalData.eps', format='eps', dpi=1000) 
